20a22244b1cf290f1735529bea3a4361bafa2a59
Shirahotsu <Shirahotsu@users.noreply.github.com>
2018-04-21 16:42:10 +0200

    0.6

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -6,27 +6,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-kategorie:string[];
+kategorie:kategorie[];
   constructor() {
     this.kategorie=[
-      "Muzyka",
-      "Gry",
-      "Dokumentalne",
-      "Moda",
-      "Sport",
-      "Motoryzacja",
-      "Filozofia",
-      "Religia",
-      "Elektronika",
-      "Vlogi",
-      "Śmieszne",
-      "Nauka",
-      "Przyroda",
-      "Gastronomia",
+      {
+        title:"Muzyka",
+        icon:"headphones"
+      },
+      {
+        title: "Gastronomia",
+        icon: "beer"
+      },
+      {
+        title: "Przyroda",
+        icon: "leaf"
+      },
+      {
+        title: "Nauka",
+        icon: "flask"
+      },
+      {
+        title: "Śmieszne",
+        icon: "smile"
+      },
+      {
+        title: "Vlogi",
+        icon: "camera"
+      },
+      {
+        title: "Elektronika",
+        icon: "microchip"
+      },
+      {
+        title: "Religia",
+        icon: "plus"
+      },
+      {
+        title: "Filozofia",
+        icon: "question"
+      },
+      {
+        title: "Motoryzacja",
+        icon: "car"
+      },
+      {
+        title: "Sport",
+        icon: "futbol"
+      },
+      {
+        title: "Moda",
+        icon: "shopping-bag"
+      },
+      {
+        title: "Dokumentalne",
+        icon: "search"
+      },
+      {
+        title: "Gry",
+        icon: "gamepad"
+      }
     ]
    }
 
   ngOnInit() {
   }
 
+}
+export interface kategorie {
+  title: string;
+  icon: string;
 }
